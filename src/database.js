990523667b7763995e597806ed929cb055bb134a
KislevBecker <kisbeckerchavez@gmail.com>
2024-06-30 21:35:24 -0600

    cambio de uri para mongod

--- src/database.js
+++ src/database.js
@@ -4,8 +4,8 @@ import {MONGODB_URI} from "./config"
 
 (async () => {
   try {
-     const db = await connect("mongodb://127.0.0.1:27017/crud-lgb");
-    //const db = await connect(MONGODB_URI);
+     //const db = await connect("mongodb://127.0.0.1:27017/crud-lgb");
+    const db = await connect("mongodb+srv://kisbeckerchavez:Ks9737-5357@cluster0.ngn9chn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("db connected", db.connection.name);
   } catch (error) {
     console.error(error);
